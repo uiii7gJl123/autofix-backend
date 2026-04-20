527bdcbf0e7d098a2158707d0b081eff71133630
uiii7gJl123 <ipod75805@outlook.sa>
2026-04-20 17:20:57 +0300

    Update createTables.js

--- createTables.js
+++ createTables.js
@@ -10,43 +10,71 @@ const client = new Client({
   ssl: { rejectUnauthorized: false }
 });
 
-async function run() {
-  await client.connect();
+await client.connect();
 
-  await client.query(`
-    CREATE TABLE IF NOT EXISTS users (
-      id SERIAL PRIMARY KEY,
-      name TEXT,
-      phone TEXT,
-      role TEXT
-    );
-  `);
+await client.query(`
+CREATE TABLE IF NOT EXISTS users(
+id SERIAL PRIMARY KEY,
+name TEXT,
+phone TEXT UNIQUE,
+password TEXT,
+role TEXT,
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+`);
 
-  await client.query(`
-    CREATE TABLE IF NOT EXISTS orders (
-      id SERIAL PRIMARY KEY,
-      client_id INTEGER,
-      workshop_id INTEGER,
-      type TEXT,
-      description TEXT,
-      status TEXT DEFAULT 'created',
-      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    );
-  `);
+await client.query(`
+CREATE TABLE IF NOT EXISTS vehicles(
+id SERIAL PRIMARY KEY,
+user_id INTEGER,
+brand TEXT,
+model TEXT,
+year INTEGER,
+plate TEXT
+);
+`);
 
-  await client.query(`
-    CREATE TABLE IF NOT EXISTS parts (
-      id SERIAL PRIMARY KEY,
-      name TEXT,
-      price NUMERIC,
-      stock INTEGER,
-      distributor_id INTEGER
-    );
-  `);
+await client.query(`
+CREATE TABLE IF NOT EXISTS workshops(
+id SERIAL PRIMARY KEY,
+user_id INTEGER,
+name TEXT,
+location TEXT,
+phone TEXT
+);
+`);
 
-  console.log("DB ready");
+await client.query(`
+CREATE TABLE IF NOT EXISTS parts(
+id SERIAL PRIMARY KEY,
+distributor_id INTEGER,
+name TEXT,
+price NUMERIC,
+stock INTEGER
+);
+`);
 
-  await client.end();
-}
+await client.query(`
+CREATE TABLE IF NOT EXISTS orders(
+id SERIAL PRIMARY KEY,
+client_id INTEGER,
+vehicle_id INTEGER,
+workshop_id INTEGER,
+description TEXT,
+status TEXT DEFAULT 'created',
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+`);
 
-run();
+await client.query(`
+CREATE TABLE IF NOT EXISTS deliveries(
+id SERIAL PRIMARY KEY,
+order_id INTEGER,
+driver_id INTEGER,
+status TEXT DEFAULT 'waiting'
+);
+`);
+
+console.log("DB ready");
+
+await client.end();
