fbfb85ad298711bd6399760110e82a5fbd70ade0
uiii7gJl123 <ipod75805@outlook.sa>
2026-04-20 17:21:36 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,9 +2,13 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
+import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
-import orderRoutes from "./routes/orderRoutes.js";
+import vehicleRoutes from "./routes/vehicleRoutes.js";
+import workshopRoutes from "./routes/workshopRoutes.js";
 import partRoutes from "./routes/partRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import deliveryRoutes from "./routes/deliveryRoutes.js";
 
 dotenv.config();
 
@@ -13,16 +17,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
-app.use("/orders", orderRoutes);
+app.use("/vehicles", vehicleRoutes);
+app.use("/workshops", workshopRoutes);
 app.use("/parts", partRoutes);
+app.use("/orders", orderRoutes);
+app.use("/delivery", deliveryRoutes);
 
-app.get("/", (req, res) => {
-  res.send("AutoFix API running");
+app.get("/", (req,res)=>{
+ res.send("AutoFix API running");
 });
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT, ()=>{
+ console.log("Server running on port " + PORT);
 });
