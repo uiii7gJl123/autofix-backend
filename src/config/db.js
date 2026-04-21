f532ffa25fa38bb81d497dbd4e2793a5678da03d
uiii7gJl123 <ipod75805@outlook.sa>
2026-04-21 22:47:17 +0300

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,15 @@
-import pkg from "pg";
+import pkg from "pg"
+import dotenv from "dotenv"
 
-const { Pool } = pkg;
+dotenv.config()
+
+const { Pool } = pkg
 
 const pool = new Pool({
- connectionString: process.env.DATABASE_URL,
- ssl: { rejectUnauthorized: false }
-});
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
 
-export default pool;
+export default pool
